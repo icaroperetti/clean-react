bee980cea3632e04ebab0600fcf3a21686348bd3
icaroperetti <icaroperetti50@gmail.com>
2021-08-28 23:39:39 -0300

    fix: removing unused file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,19 @@
-import { HttpPostClient } from './../../protocols/http/http-post-client'
-import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClient } from "./../../protocols/http/http-post-client";
+import { RemoteAuthentication } from "./remote-authentication";
 
 describe("RemoteAuthentication", () => {
   test("Should call HttpPostClient with correct url", async () => {
     class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post(url:string):Promise<void> {
-        this.url = url
-        return Promise.resolve()
+      url?: string;
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
       }
     }
     const url = "any_url";
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url,httpPostClient)
-    await sut.auth()
+    const httpPostClient = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClient);
+    await sut.auth();
     expect(httpPostClient.url).toBe(url);
   });
 });
